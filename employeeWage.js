0f98087fbcf647abce2b824012dd75b5175e46dd
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-03 18:43:29 +0530

    [Rakshit] Add:UC2 employee wage

--- employeeWage.js
+++ employeeWage.js
@@ -1,8 +1,26 @@
 //UC1
-const random = Math.floor(Math.random()*2);
+// const random = Math.floor(Math.random()*2);
 
-if(random === 1){
-    console.log("Employee is present");
-}else{
-    console.log("Employee is absent");
+// if(random === 1){
+//     console.log("Employee is present");
+// }else{
+//     console.log("Employee is absent");
+// }
+
+//UC2
+const random = Math.floor(Math.random()*3)+1;
+let wage
+switch(random){
+    case 1:
+        wage = 8*20;
+        break;
+    case 2:
+        wage = 4*20;
+        break;
+    default:
+        wage = 0;
+        break;
 }
+
+console.log("Employee wage is: "+wage);
+
