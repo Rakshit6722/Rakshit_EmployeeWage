5489423be595c66676ce3d91f7190464c331cbc9
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-03 18:48:37 +0530

    [Rakshit] Add: UC3 employee wage

--- employeeWage.js
+++ employeeWage.js
@@ -8,19 +8,40 @@
 // }
 
 //UC2
-const random = Math.floor(Math.random()*3)+1;
-let wage
-switch(random){
-    case 1:
-        wage = 8*20;
-        break;
-    case 2:
-        wage = 4*20;
-        break;
-    default:
-        wage = 0;
-        break;
+// const random = Math.floor(Math.random()*3)+1;
+// let wage
+// switch(random){
+//     case 1:
+//         wage = 8*20;
+//         break;
+//     case 2:
+//         wage = 4*20;
+//         break;
+//     default:
+//         wage = 0;
+//         break;
+// }
+
+// console.log("Employee wage is: "+wage);
+
+//UC3
+const workHours = () => {
+    const random = Math.floor(Math.random()*3)+1;
+    let hours
+    switch(random){
+        case 1:
+            hours = 8;
+            break;
+        case 2:
+            hours = 4;
+            break;
+        default:
+            hours = 0;
+            break;
+    }
+    return `${hours} working hours`;
 }
 
-console.log("Employee wage is: "+wage);
+const workingHours = workHours()
+console.log(workingHours)
 
