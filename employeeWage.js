4e4c1e214ecf4c92d6da966c02ddf6df3445edcf
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-04 11:37:03 +0530

    [Rakshit] Refactor: UC1 employee wage

--- employeeWage.js
+++ employeeWage.js
@@ -1,8 +1,5 @@
 //UC1
-const random = Math.floor(Math.random()*2);
-
-if(random === 1){
-    console.log("Employee is present");
-}else{
-    console.log("Employee is absent");
+const getAttendance = () => {
+    const random = Math.floor(Math.random() * 2);
+    return random === 1 ? 'Present' : 'Absent';
 }
